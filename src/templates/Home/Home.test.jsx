95f8497548475fd656b7db8059247b45c925b6da
Lucas Leite <carvalho1104@hotmail.com>
2021-04-04 19:17:03 -0300

    build

---
+++ src/templates/Home/Home.test.jsx
@@ -0,0 +1,16 @@
+
+describe('<Home />', () => {
+  test('test one', () => {
+    expect(1).toBe(1);
+  });
+
+  test('test two', () => {
+    expect(1).toBe(1); 
+  });
+
+  test('test three', () => {
+    expect(1).toBe(1);
+  });
+ });
+
+
